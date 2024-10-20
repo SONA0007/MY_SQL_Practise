81922e5482471c65100414429d689ea7c99e62a2
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2024-10-20 16:59:31 +0530

    Update operators.sql

--- operators.sql
+++ operators.sql
@@ -7,3 +7,18 @@ Not (ord_date = '2012-09-10'
 And  salesman_id > 5005) 
 or   purch_amt > 1000;              
 
+-- 10. Write a SQL query that displays order number, purchase amount, and the achieved and unachieved percentage (%) for those orders that exceed 50% of the target value of 6000.  
+
+SELECT ord_no, purch_amt,
+(100 * purch_amt) / 6000 AS "Achieved %", 
+
+(100 * (6000 - purch_amt) / 6000) AS "Unachieved %"
+
+FROM orders 
+WHERE (100 * purch_amt) / 6000 > 50;
+
+-- 11. From the following table, write a SQL query to find the details of all employees whose last name is ‘Dosni’ or ‘Mardy’. Return emp_idno, emp_fname, emp_lname, and emp_dept. 
+SELECT * FROM  emp_details
+WHERE 
+EMP_LNAME ='Dosni' or EMP_LNAME='Mardy';
+

440b94ea1b7822789ee441aa92d59c844044d898
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2024-10-21 18:34:32 +0530

    Update operators.sql

--- operators.sql
+++ operators.sql
@@ -22,3 +22,10 @@ SELECT * FROM  emp_details
 WHERE 
 EMP_LNAME ='Dosni' or EMP_LNAME='Mardy';
 
+-- write a  SQL query to select orders between 500 and 4000 (begin and end values are included). Exclude orders amount 948.50 and 1983.43. Return ord_no, purch_amt, ord_date, customer_id, and salesman_id. Sample table: orders
+SELECT * FROM 
+ORDERS
+where
+   (purch_amt BETWEEN 500 AND 4000)
+AND 
+   purch_amt NOT IN (948.50, 1983.43); 

d8e694cfd8a29fdf628c5e6e82511b0d380d4286
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2024-10-26 16:48:28 +0530

    Create Aggregate_Fun.sql

---
+++ Aggregate_Fun.sql
@@ -0,0 +1,41 @@
+-- write a SQL query that counts the number of unique salespeople. Return number of salespeople.  Sample table: orders
+select count(distinct salesman_id) as count from orders;
+
+-- write a  SQL query to determine the number of customers who received at least one grade for their activity.
+-- way1
+select count(distinct customer_id) as count
+from customer
+where grade is not null;
+-- way2
+select counnt(All grade) from customer; 
+
+-- write a SQL query to find the highest grade of the customers in each city. Return city, maximum grade. Sample table: customer
+SELECT city, max(grade) as max FROM customer
+group by city ;
+
+-- write a SQL query to find the highest purchase amount ordered by each customer. Return customer ID, maximum purchase amount. Sample table: orders
+SELECT customer_id, max(purch_amt) as max
+FROM orders
+group by customer_id
+order by customer_id ;
+
+--  write a SQL query to find the highest purchase amount ordered by each customer on a particular date. Return, order date and highest purchase amount.Sample table: orders
+SELECT customer_id, ord_date, MAX(purch_amt) 
+FROM orders
+group by customer_id, ord_date ;
+
+-- -- From the following table, write a SQL query to determine the highest purchase amount made by each salesperson on '2012-08-17'. 
+-- Return salesperson ID, purchase amount . Sample table: orders
+
+-- ord_no      purch_amt   ord_date    customer_id  salesman_id
+-- ----------  ----------  ----------  -----------  -----------
+-- 70001       150.5       2012-10-05  3005         5002
+-- 70009       270.65      2012-09-10  3001         5005
+-- 70002       65.26       2012-10-05  3002         5001
+-- 70004       110.5       2012-08-17  3009         5003
+-- 70007       948.5       2012-09-10  3005         5002
+
+SELECT salesman_id,MAX(purch_amt) 
+FROM orders
+where ord_date ='2012-08-17'
+group by salesman_id, ord_date ;

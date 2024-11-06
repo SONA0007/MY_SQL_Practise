e6c607efa8313c77912394195f5a6d0ae28d1428
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2024-11-06 18:46:49 +0530

    Update Query_on_multiple_table.sql

--- Query_on_multiple_table.sql
+++ Query_on_multiple_table.sql
@@ -19,5 +19,27 @@ SELECT o.ord_no, c.cust_name, o.customer_id, o.salesman_id
 FROM salesman s, customer c, orders o
 WHERE
     s.city != c.city
+ -- you can use s.city <> c.city
 and o.customer_id = c.customer_id
 and o.salesman_id = s.salesman_id;
+
+--  SQL query to find those customers where each customer has a grade and is served by a salesperson who belongs to a city.
+-- Return cust_name as "Customer", grade as "Grade".
+SELECT  c.cust_name as "Customer", c.grade as "Grade", o.ord_no as "Order No"
+FROM  customer c, orders o, salesman s
+WHERE
+     c.grade is not null 
+and  s.city is not null
+and o.customer_id = c.customer_id
+and o.salesman_id = s.salesman_id;
+
+
+-- write a SQL query to find those customers who are served by a salesperson and the salesperson earns commission in the range of 12% to 14% 
+-- (Begin and end values are included.). Return cust_name AS "Customer", city AS "City". 
+
+SELECT  c.cust_name as "Customer", s.name, c.city as "City", s.commission
+FROM  customer c, salesman s
+WHERE
+    s.commission between 0.12 and 0.14
+and c.salesman_id = s.salesman_id;
+-- 

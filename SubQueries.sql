b1adcfbeb22c435380545dd271536de92221ed0d
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2024-11-10 23:43:56 +0530

    Create SubQueries.sql

---
+++ SubQueries.sql
@@ -0,0 +1,4 @@
+https://www.w3resource.com/sql-exercises/subqueries/index.php
+
+-- From the following tables, write a  SQL query to find all the orders issued by the salesman 'Paul Adam'. 
+-- Return ord_no, purch_amt, ord_date, customer_id and salesman_id.
